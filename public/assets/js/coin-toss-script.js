f565cc6ddd077d779f451560afb36654c668c0c9
EvilDogma <512gallagher@gmail.com>
2024-05-01 19:10:29 -0400

    rollpageworks

--- public/assets/js/coin-toss-script.js
+++ public/assets/js/coin-toss-script.js
@@ -1,16 +1,53 @@
-const coinIcon = document.getElementById('coin'); 
+const coinIcon = document.getElementById('coin');
+const betForm = document.getElementById("bet-form") 
 const tossBtn = 
-	document.getElementById('toss-button'); 
+	document.getElementById('bet-btn'); 
 const result = 
 	document.querySelector('.result'); 
-coinIcon.insertAdjacentElement('afterend', result); 
-tossBtn.addEventListener('click', () => { 
-	tossBtn.disabled = true; 
-	tossCoinFunction(); 
-}); 
-function tossCoinFunction() { 
-	const randomVal = Math.random(); 
-	const faceCoin = randomVal < 0.5 ? 'Heads' : 'Tails'; 
+coinIcon.insertAdjacentElement('afterend', result) 
+tossBtn.addEventListener('click', tossCoinFunction) 
+async function tossCoinFunction(event) { 
+	event.preventDefault()
+    // make new route to return event
+    const eventObj = {
+      event_name: "coinflip",
+      odds: {
+          heads: 0.50,
+          tails: 0.50
+      }
+  }
+    const user_id = betForm.dataset.user
+    const newEvent =  await fetch('api/events', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(eventObj)
+    })
+    .then(res=>res.json())
+  const event_id = newEvent.id
+  const bet_name = document.querySelector('input[name="bet_name"]:checked').value
+const amount = document.getElementById("amount").value
+    const betObj = {
+      user_id: user_id,
+      event_id: event_id,
+      bet_name: bet_name,
+      amount: amount,
+      odds: 0.50
+    }
+    console.log(betObj)
+
+    const bet =  await fetch('api/bets/bet', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(betObj),
+      })
+      .then(res=>res.json())
+    console.log(bet)
+
+	const faceCoin = bet.result == 'heads' ? 'Heads' : 'Tails'; 
 	const imageUrl = faceCoin === 'Heads' ? 
 '/assets/images/coin-heads.png' : 
 '/assets/images/coin-tails.png'; 
