abf2ec6330cb542cb6d533cc013b026c7d13d605
EvilDogma <512gallagher@gmail.com>
2024-04-30 23:17:02 -0400

    updated partials to pass parameters

--- routes/view_routes.js
+++ routes/view_routes.js
@@ -1,9 +1,9 @@
 const router = require('express').Router()
 
-const {User,Event,Bet} = require('../models')
+const { User, Event, Bet } = require('../models')
 
-function isAuth(req,res){
-    if(!req.session.user_id){
+function isAuth(req, res) {
+    if (!req.session.user_id) {
         return false
     }
     return true
@@ -18,22 +18,24 @@ router.get('/about', (req, res) => {
     res.render('about')
 })
 
-router.get('/play', async(req, res) => {
-    const auth = isAuth(req,res)
+router.get('/play', async (req, res) => {
+    const auth = isAuth(req, res)
     let userObj = {
         isLoggedIn: false
     }
-    if (auth){
+    if (auth) {
         const user = await User.findByPk(req.session.user_id)
         userObj = {
             isLoggedIn: true,
-            vars:{username: user.username,
-                balance: user.balance}
+            vars: {
+                username: user.username,
+                balance: user.balance
+            }
         }
-    
+
     }
     res.render('play', userObj)
-    
+
 })
 
 router.get('/register', (req, res) => {
